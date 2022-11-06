9307bcdda2bddd9304c49ab8446157dde5ebe7b3
hzxadminroot <476148585@qq.com>
2022-11-06 20:44:53 +0800

    添加自定义响应类型

---
+++ yellow_b_dao/src/main/java/com/yellow/b/domain/Result.java
@@ -0,0 +1,42 @@
+package com.yellow.b.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Result {
+    private String code;
+    private String msg;
+    private Object result;
+
+    public Result(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+    public Result(String code, String msg,Object data) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static Result ok(){
+        return new Result("200","成功");
+    }
+    public static Result ok(String code,String msg){
+        return new Result(code,msg);
+    }
+    public static Result ok(Object data){
+        return new Result("200","成功",data);
+    }
+    public static Result ok(String code,String msg,Object data){
+        return new Result(code,msg,data);
+    }
+    public static Result fail(){
+        return new Result("201","失败");
+    }
+    public static Result fail(String code,String msg){
+        return new Result(code,msg);
+    }
+}
