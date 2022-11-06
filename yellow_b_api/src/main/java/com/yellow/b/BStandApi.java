2f6390843eb8b35979c5658b613f1a6f4ce6cf98
hzxadminroot <476148585@qq.com>
2022-11-06 19:23:31 +0800

    添加api子模块的spring boot启动类

---
+++ yellow_b_api/src/main/java/com/yellow/b/BStandApi.java
@@ -0,0 +1,11 @@
+package com.yellow.b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BStandApi {
+    public static void main(String[] args) {
+        SpringApplication.run(BStandApi.class,args);
+    }
+}
--- yellow_b_dao/src/main/java/com/yellow/b/BStandDao.java
+++ yellow_b_dao/src/main/java/com/yellow/b/BStandDao.java
@@ -1,6 +1,6 @@
 package com.yellow.b;
 
-public class Main {
+public class BStandDao {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
